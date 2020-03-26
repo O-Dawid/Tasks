9b8de96279ad2e59aa3c8d1e47aebeae3ae1f58e
Dawid <dawid.olesinski00@gmail.com>
2020-03-26 21:17:05 +0100

    second

--- NumberOfIslands/src/app/App.java
+++ NumberOfIslands/src/app/App.java
@@ -4,32 +4,33 @@ public class App {
 
     static public int numIslands(char[][] grid) {
         int count = 0;
-        boolean [][]visited = new boolean[grid.length][grid[0].length];
         for (int row = 0; row < grid.length; row++) {
             for (int column = 0; column < grid[row].length; column++) {
                 if (grid[row][column] == '1') {
-                    visited[row][column] = true;
                     count++;
-                    callDFS(grid, row, column, visited);
+                    callDFS(grid, row, column);
                 }
             }
         }
         return count;
     }
 
-    static public void callDFS(char[][] grid, int row, int column, boolean[][] visited) {
-        if (isSafe(grid, row, column)) {
+    static public void callDFS(char[][] grid, int row, int column) {
+        if (isOutOfRange(grid, row, column)) {
             return;
         }
-        grid[row][column] = '0';
-        visited[row][column] = true;
-        callDFS(grid, row + 1, column, visited);
-        callDFS(grid, row - 1, column, visited);
-        callDFS(grid, row, column - 1, visited);
-        callDFS(grid, row, column + 1, visited);
+        markSpot(grid, row, column);
+        callDFS(grid, row + 1, column);
+        callDFS(grid, row - 1, column);
+        callDFS(grid, row, column - 1);
+        callDFS(grid, row, column + 1);
     }
 
-    private static boolean isSafe(char[][] grid, int row, int column) {
+    private static void markSpot(char[][] grid, int row, int column) {
+        grid[row][column] = '0';
+    }
+
+    private static boolean isOutOfRange(char[][] grid, int row, int column) {
         return row < 0 || row >= grid.length || column < 0 || column >= grid[row].length || grid[row][column] == '0';
     }
 
